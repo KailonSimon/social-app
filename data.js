4d89ece8c4d66b54c64534ec911089cdc846ac4c
Kailon Simon <kailonsimon@gmail.com>
2022-02-15 02:15:48 -0600

    Switched placeholders

--- data.js
+++ data.js
@@ -1,79 +1,79 @@
 const posts = [
     {
         id: 1,
-        displayName: "Rick Rock",
-        username: "rickRock122",
-        text: "heyeheye",
+        displayName: "Placeholder",
+        username: "username11",
+        text: "Post Text",
         date: "10h"
     },
     {   
         id: 2,
-        displayName: "Rick Rock",
-        username: "rickRock122",
-        text: "heyeheye",
+        displayName: "Placeholder",
+        username: "username11",
+        text: "Post Text",
         date: "22m"
     },
     {
         id: 3,
-        displayName: "Rick Rock",
-        username: "rickRock122",
-        text: "heyeheye",
+        displayName: "Placeholder",
+        username: "username11",
+        text: "Post Text",
         date: "11h"
     },
     {
         id: 4,
-        displayName: "Rick Rock",
-        username: "rickRock122",
-        text: "heyeheye",
+        displayName: "Placeholder",
+        username: "username11",
+        text: "Post Text",
         date: "4h"
     },
     {
         id: 5,
-        displayName: "Rick Rock",
-        username: "rickRock122",
-        text: "heyeheye",
+        displayName: "Placeholder",
+        username: "username11",
+        text: "Post Text",
         date: "18m"
     },
     {
         id: 6,
-        displayName: "Rick Rock",
-        username: "rickRock122",
-        text: "heyeheye",
+        displayName: "Placeholder",
+        username: "username11",
+        text: "Post Text",
         date: "3m"
     },
     {
         id: 7,
-        displayName: "Rick Rock",
-        username: "rickRock122",
-        text: "heyeheye",
+        displayName: "Placeholder",
+        username: "username11",
+        text: "Post Text",
         date: "38m"
     },
     {
         id: 8,
-        displayName: "Rick Rock",
-        username: "rickRock122",
-        text: "heyeheye",
+        displayName: "Placeholder",
+        username: "username11",
+        text: "Post Text",
         date: "5m"
     },
     {
         id: 9,
-        displayName: "Rick Rock",
-        username: "rickRock122",
-        text: "heyeheye",
+        displayName: "Placeholder",
+        username: "username11",
+        text: "Post Text",
         date: "1h"
     },
     {
         id: 10,
-        displayName: "Rick Rock",
-        username: "rickRock122",
-        text: "heyeheye",
+        displayName: "Placeholder",
+        username: "username11",
+        text: "Post Text",
         date: "7h"
     },
     {
         id: 11,
-        displayName: "Rick Rock",
-        username: "rickRock122",
-        text: "heyeheye",
+        displayName: "Placeholder",
+        username: "username11",
+        text: "Post Text",
         date: "24m"
     },
 ]
