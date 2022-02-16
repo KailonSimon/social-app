aa524910f784683d260ed2011e46d0c000221eb6
Kailon Simon <kailonsimon@gmail.com>
2022-02-15 22:46:12 -0600

    Added authentication conditional

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,28 @@
+import { useState } from 'react'
 import Head from 'next/head'
-import Image from 'next/image'
 import BottomNav from '../src/components/BottomNav'
 import Feed from '../src/components/Feed'
 import Navbar from '../src/components/Navbar'
 import NavDrawer from '../src/components/NavDrawer'
 import styles from '../styles/Home.module.css'
+import Login from '../src/components/Login'
 
 export default function Home() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   return (
     <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      {isAuthenticated ? 
+      <>
       <Navbar />
       <Feed />
       <BottomNav />
+      </>
+      :
+      <Login />
+      } 
     </>
       
   )
