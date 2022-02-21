d99462dacbe9ff58a2f16be0bde0223ecad36196
Kailon Simon <kailonsimon@gmail.com>
2022-02-20 18:47:16 -0600

    Made created user doc id match uid

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,6 +1,6 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import {GoogleAuthProvider, getAuth, signInWithPopup, signInWithEmailAndPassword, createUserWithEmailAndPassword, sendPasswordResetEmail, signOut } from "firebase/auth";
-import {getFirestore, query, getDocs, collection, where, addDoc } from "firebase/firestore";
+import {getFirestore, query, getDocs, collection, where, setDoc, doc } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -27,12 +27,16 @@ const signInWithGoogle = async () => {
     const q = query(collection(db, "users"), where("uid", "==", user.uid));
     const docs = await getDocs(q);
     if (docs.docs.length === 0) {
-      await addDoc(collection(db, "users"), {
+      await setDoc(doc(db, "users", user.uid), {
         uid: user.uid,
         name: user.displayName,
         authProvider: "google",
         email: user.email,
-        avatar: user.photoURL
+        avatar: user.photoURL,
+        posts: [],
+        favorites: [],
+        followers: [],
+        following: []
       });
     }
   } catch (error) {
