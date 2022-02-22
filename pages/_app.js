309a34f04462bd5fec1e7a41cdb5bd2bc28b7528
Kailon Simon <kailonsimon@gmail.com>
2022-02-22 04:35:56 -0600

    Added nextauth and recoil providers

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,18 @@
 import '../styles/globals.css'
 import { ThemeProvider } from '@mui/material/styles';
-import { Provider, useDispatch } from 'react-redux';
-import { store } from '../src/features/redux/store';
 import { darkTheme } from '../styles/MUIthemes';
+import { SessionProvider } from 'next-auth/react';
+import { RecoilRoot } from 'recoil';
 
-
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={darkTheme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </Provider>
+    <SessionProvider session={session}>
+      <RecoilRoot>
+        <ThemeProvider theme={darkTheme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </RecoilRoot>
+    </SessionProvider>
   )
 }
 
