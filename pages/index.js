69231171d42cba343d4e03b84b2020fc495a1b5c
Kailon Simon <kailonsimon@gmail.com>
2022-02-22 17:34:06 -0600

    Added redirect if unauthorized

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,21 @@ import Head from 'next/head'
 import Feed from '../src/components/Feed'
 import Navbar from '../src/components/Navbar'
 import { CircularProgress, Container } from '@mui/material'
+import { useRouter } from "next/router"
 
 
 export default function Home() {
   const { data: session, status } = useSession()
-
+  const router = useRouter();
   if (status === "loading") {
     return (
       <Container sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'background.paper', height: '100vh'}} disableGutters>
         <CircularProgress />
       </Container>
     )
+  }
+  if (status === "unauthenticated") {
+    router.push('/auth/signin')
   }
 
   return (
