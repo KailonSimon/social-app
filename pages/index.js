7a6e9488b272db16b19d67945cf08ea977d4f427
Kailon Simon <kailonsimon@gmail.com>
2022-02-22 15:13:44 -0600

    Added loading state

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
+import { useSession } from "next-auth/react"
 import Head from 'next/head'
 import Feed from '../src/components/Feed'
 import Navbar from '../src/components/Navbar'
-import { Container } from '@mui/material'
+import { CircularProgress, Container } from '@mui/material'
 
 
 export default function Home() {
+  const { data: session, status } = useSession()
+
+  if (status === "loading") {
+    return (
+      <Container sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'background.paper', height: '100vh'}} disableGutters>
+        <CircularProgress />
+      </Container>
+    )
+  }
 
   return (
     <>
